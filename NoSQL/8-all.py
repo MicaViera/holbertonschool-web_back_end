9e190b62dccbf78c1ba660bc625a67b06840dbd9
MicaViera <vieranoelia98@gmail.com>
2024-01-11 19:22:27 -0800

    Function that lists all documents in a collection of mongoDB.

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Function that lists all documents in a collection of mongoDB."""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """Function that returns a lists all documents."""
+    all_docs = mongo_collection.find()
+    if not all_docs:
+        return []
+    else:
+        return documents
