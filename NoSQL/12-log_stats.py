f02c4edcdd6121bb6d1afce94cddfce5c0f8d4fd
MicaViera <vieranoelia98@gmail.com>
2024-01-13 04:30:01 +0000

    Fixing script that provides some stats about Nginx logs stored in MongoDB.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,22 +2,23 @@
 """Script that provides some stats about Nginx logs stored in MongoDB."""
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    num_documents = nginx_collection.countDocuments()
+    num_documents = nginx_collection.count_documents({})
     print(f"{num_documents} logs")
     print("Methods:")
-    num_GET = nginx_collection.countDocuments({"method": "GET"})
+    num_GET = nginx_collection.count_documents({"method": "GET"})
     print(f"/tmethod GET:{num_GET}")
-    num_POST = nginx_collection.countDocuments({"method": "POST"})
+    num_POST = nginx_collection.count_documents({"method": "POST"})
     print(f"/tmethod GET:{num_POST}")
-    num_PUT = nginx_collection.countDocuments({"method": "PUT"})
+    num_PUT = nginx_collection.count_documents({"method": "PUT"})
     print(f"/tmethod GET:{num_PUT}")
-    num_PATCH = nginx_collection.countDocuments({"method": "PATCH"})
+    num_PATCH = nginx_collection.count_documents({"method": "PATCH"})
     print(f"/tmethod GET:{num_PATCH}")
-    num_DELETE = nginx_collection.countDocuments({"method": "DELETE"})
+    num_DELETE = nginx_collection.count_documents({"method": "DELETE"})
     print(f"/tmethod GET:{num_DELETE}")
-    num_status = nginx_collection.countDocuments({"path": "/status"})
+    num_status = nginx_collection.count_documents({"path": "/status"})
     print(f"{num_status} status check")
