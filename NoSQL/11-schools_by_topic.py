0b7fbc2fefb28bff6c64e534568a66ef6e72b7dc
MicaViera <vieranoelia98@gmail.com>
2024-01-13 02:40:10 +0000

    Function that returns the list of school having a specific topic.

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that returns the list of school having a specific topic."""
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """"Function that returns the list."""
+    return mongo_collection.find({"topic": topic})
