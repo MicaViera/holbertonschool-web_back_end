d421067e3a34846eeb1995018b0fc95dcc17c5af
MicaViera <vieranoelia98@gmail.com>
2024-01-14 01:38:51 +0000

    Function named index_range that takes two integer arguments page and page_size.

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@ and page_size."""
 from typing import Tuple
 
 
-def index_range(page: int, page_size:int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Function that returns a tuple."""
     total_size = page * page_size
     initial_size = total_size - page_size
