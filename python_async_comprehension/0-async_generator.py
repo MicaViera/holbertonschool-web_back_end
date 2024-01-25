8da670430e2170d29088515eb5827004724a1b54
MicaViera <vieranoelia98@gmail.com>
2024-01-24 19:10:49 -0800

    Function that loops 10 times asynchronously.

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Write a coroutine called async_generator that takes no arguments."""
+import random
+import asyncio
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Function that loops 10 times asynchronously."""
+    for index in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
