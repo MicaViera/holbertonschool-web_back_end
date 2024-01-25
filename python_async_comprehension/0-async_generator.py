d00d370e62dc7c5d6c0f2d47a310d4b63aae1dc2
MicaViera <vieranoelia98@gmail.com>
2024-01-24 19:13:41 -0800

    Function that loops 10 times asynchronously.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,6 @@ import asyncio
 from typing import Generator
 
 
-
 async def async_generator() -> Generator[float, None, None]:
     """Function that loops 10 times asynchronously."""
     for index in range(10):
